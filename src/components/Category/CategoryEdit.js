6ff5a82757ba24071ee54b37b3a05fec179ec325
oren <elador1@gmail.com>
2016-08-29 17:41:49 +0300

    start work on edit

--- src/components/Category/CategoryEdit.js
+++ src/components/Category/CategoryEdit.js
@@ -2,13 +2,87 @@
  * Created by oren on 8/28/16.
  */
 import React from 'react';
+import * as categoriesActions from '../../actions/categoriesActions';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MainToolbar from '../MainToolbar';
+import { browserHistory } from 'react-router'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {Map} from 'immutable';
 
+
+function mapStateToProps(state) {
+  return {
+    categoriesList: state.categories.get("categories"),
+    currentId: state.categories.get("currentId")
+  };
+}
+
+const actions = [categoriesActions];
+
+function mapDispatchToProps(dispatch) {
+  const creators = Map()
+    .merge(...actions)
+    .filter(value => typeof value === 'function')
+    .toObject();
+
+  return {
+    actions: bindActionCreators(creators, dispatch),
+    dispatch
+  };
+}
 
 class CategoryEdit extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      addValue: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({addValue: event.target.value});
+  };
+
+  handleClick = () => {
+    let categoriesLocal = JSON.parse(localStorage.getItem('Categories')),
+      self = this,
+      hasValue = false;
+      self.props.actions.editCategory(this.props.currentId,self.state.addValue);
+    _.forEach(categoriesLocal, function(value,key){
+      if(self.state.addValue == value){
+        alert("The Category exist, Please add a new one");
+        hasValue = true;
+      }
+    });
+    localStorage.setItem('AddedCategory', [this.state.addValue]);
+    !hasValue && browserHistory.goBack();
+  };
+
   render() {
-    return (<div><h1>Edit Component</h1></div>)
+    return (<div>
+      <MainToolbar disabledButtons={true}/>
+      <div className="category-add-component">
+        <h1>Edit Category</h1>
+        <div className="category-add">
+          <MuiThemeProvider>
+            <TextField
+              hintText="Name"
+              floatingLabelText="Edit Category Name"
+              defaultValue = {this.props.categoriesList.get(this.props.currentId)}
+              onChange={this.handleChange}/>
+          </MuiThemeProvider>
+          <MuiThemeProvider>
+            <FlatButton label="Save" primary={true} onClick={this.handleClick}/>
+          </MuiThemeProvider>
+        </div>
+      </div>
+    </div>)
   }
 
 }
 
-export default CategoryEdit;
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryEdit);
